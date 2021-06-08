51b5c90b0270fc9ed41c99f99d2846d13b741f07
FranciscoMicolta <franciscomicolta@hotmail.com>
2021-06-08 08:55:21 -0500

    Add project files.

---
+++ University.App/University.App/ViewModels/Forms/PQRSViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace University.App.ViewModels.Forms
+{
+    public class PQRSViewModel : BaseViewModel
+    {
+        #region Attributes
+        public class TypeRequest
+        {
+            public string Name { get; set; }
+        }
+
+        public class RateService
+        {
+            public string Point { get; set; }
+        }
+
+        private List<TypeRequest> _typeRequests;
+        private List<RateService> _rateService;
+        #endregion
+
+        #region Properties
+        public List<TypeRequest> TypeRequests
+        {
+            get { return this._typeRequests; }
+            set { this.SetValue(ref this._typeRequests, value); }
+        }
+        public List<RateService> RateServices
+        {
+            get { return this._rateService; }
+            set { this.SetValue(ref this._rateService, value); }
+        }
+        #endregion
+
+        #region Constructor
+        public PQRSViewModel()
+        {
+            this.LoadTypeRequest();
+            this.LoadPuntuation();
+        }
+        #endregion
+
+        #region Methods
+        private void LoadTypeRequest()
+        {
+            this.TypeRequests = new List<TypeRequest>
+            {
+                new TypeRequest { Name = "Petition" },
+                new TypeRequest { Name = "Complain" },
+                new TypeRequest { Name = "Claim" },
+                new TypeRequest { Name = "Suggestion" }
+            };
+        }
+
+        private void LoadPuntuation()
+        {
+            this.RateServices = new List<RateService>
+            {
+                new RateService { Point = "Bad" },
+                new RateService { Point = "Regular" },
+                new RateService { Point = "Well" },
+                new RateService { Point = "Accetable" },
+                new RateService { Point = "Excellent" }
+            };
+        }
+        #endregion
+    }
+}
---
+++ University.BL/Helpers/Utils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.BL.Helpers
+{
+    public class Utils
+    {
+        public static void SaveFile(string path,
+            string base64BinaryStr)
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+
+            byte[] tempByte = Convert.FromBase64String(base64BinaryStr);
+            FileStream file = File.Create(path);
+            file.Write(tempByte, 0, tempByte.Length);
+            file.Close();
+        }
+
+        public static bool IsExistFile(string path)
+        {
+            if (File.Exists(path)) return true;
+            return false;
+        }
+    }
+}
